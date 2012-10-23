3859417d63b79abd528b5ed88c42c4b0d5b51f4e
Adrian Holzer <adrian.holzer@gmail.com>
2012-10-23 16:58:20 +0200

    Initial Commit

---
+++ SpeakUp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpeakUp
+//
+//  Created by Adrian Holzer on 23.10.12.
+//  Copyright (c) 2012 Adrian Holzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
